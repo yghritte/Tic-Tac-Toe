2250a010f952d6d4101a7b5ae264ea9100ecfc43
yghritte <padziana@gmail.com>
2020-09-22 14:19:46 +0200

    Created medium-level

--- TicTacToe.py
+++ TicTacToe.py
@@ -28,7 +28,7 @@ def print_board(cells):
     print("---------")
 
 
-def states(cells):
+def status(cells):
     winning_positions = [cells[0:3], cells[3:6], cells[6:9],       # horizontal
                         cells[0:7:3], cells[1:8:3], cells[2:9:3],  # vertical
                         cells[0:9:4], cells[2:7:2]]                # diagonal
@@ -73,19 +73,58 @@ def user_move(cells):
                 print("Coordinates should be from 1 to 3!")
 
 
-def AI_easy_move(cells):
-    print('Making move level "easy"')
+def AI_easy_move(cells, info = True):
+    if info is True:
+        print('Making move level "easy"')
     allowed_pos = [i for i in range(9) if cells[i] == " "]
     random_point = random.choice(allowed_pos)
     cells[random_point] = players_char(cells)
     print_board(cells)
 
 
+def win_in_one(cells):
+    if players_char(cells) == 'O':
+        player_sign = 'O'
+        enemy_sign = 'X'
+    else:
+        player_sign = 'X'
+        enemy_sign = 'O'
+
+    for i in range(9):
+        # if cell is empty, let's put there a char and check if it gives a win
+        if cells[i] == " ":
+
+            cells[i] = enemy_sign
+            if status(cells) in ['X wins', 'O wins']:
+                cells[i] = " "
+                return True, i
+
+            cells[i] = player_sign
+            if status(cells) in ['X wins', 'O wins']:
+                cells[i] = " "
+                return True, i
+            
+            cells[i] = " "
+
+    return False, None
+
+
 def AI_medium_move(cells):
-    pass
+    print('Making move level "medium"')
+    win, pos = win_in_one(cells)
+    print(win, pos)
+    if win:
+        print(cells)
+        cells[pos] = players_char(cells)
+        print_board(cells)
+    
+    else:
+        AI_easy_move(cells, False)
+
 
 
 def AI_hard_move(cells):
+    print('Making move level "hard"')
     pass
 
 
@@ -93,24 +132,36 @@ def start_game(player1, player2):
     cells = [" "] * 9
     print_board(cells)
 
-    while states(cells) == "Game not finished":
+    while status(cells) == "Game not finished":
         if player1 == 'user':
             user_move(cells)
         
-        else:
+        elif player1 == 'easy':
             AI_easy_move(cells)
+        
+        elif player1 == 'medium':
+            AI_medium_move(cells)
+        
+        else:
+            AI_hard_move(cells)
 
         # checking if the game is over
-        if states(cells) != "Game not finished":
+        if status(cells) != "Game not finished":
             break
         
         if player2 == 'user':
             user_move(cells)
 
-        else:
+        elif player2 == 'easy':
             AI_easy_move(cells)
+        
+        elif player2 == 'medium':
+            AI_medium_move(cells)
+        
+        else:
+            AI_hard_move(cells)
 
-    print(states(cells))
+    print(status(cells))
 
 
 def main():
@@ -128,7 +179,7 @@ def main():
             print("Bad parameters!")
 
         else:
-            if menu[0] == 'start' and menu[1] in ['user','easy'] and menu[2] in ['user','easy']:
+            if menu[0] == 'start' and all(x in ['user','easy', 'medium', 'hard'] for x in [menu[1], menu[2]]):
                 player1 = menu[1]
                 player2 = menu[2]
 
